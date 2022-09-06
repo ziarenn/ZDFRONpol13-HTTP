bcded3c5ea58edb312fff6dc4a9019636d08a411
Kacper Żarna <kacper-zarna@wp.pl>
2022-09-06 18:30:06 +0200

    Kod 06.09.2022 18:30

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const fetchJSON = () => {
     console.log(jsonString);
   };
 };
-fetchJSON();
+// fetchJSON();
 // JEŻELI DOSTAJESZ RESPONSE OD SERWERA, TO DANE BĘDĄ W JSON
 // ŻEBY PRACOWAĆ NA TYCH DANYCH W JS, MUSIMY NAJPIERW PRZEROBIĆ JSON NA ZWYKŁY JSOWY OBIEKT
 // JSON.parse()
@@ -20,9 +20,48 @@ fetchJSON();
 // JEŻELI WYSYŁASZ DANE NA SERWER, TO MUSISZ ZADBAĆ O TO, ŻEBY WYSŁANE DANE NIE BYŁY OBIEKTEM JSOWYM, TYLKO STRINGIEM JSONOWYM
 // JSON.stringify()
 
-const x = {
-  email: "1213@123.pl",
-  password: "12343",
-};
+// const x = {
+//   email: "1213@123.pl",
+//   password: "12343",
+// };
 // JSON.stringify()
 // wysyłanie
+
+// BLOK HTTP ZDFRONpol13
+
+// METODY HTTP
+
+// 1. Metoda GET
+
+// a) XMLHttpRequest
+// http://jsonplaceholder.typicode.com/todos/
+const XMLRequestGET = () => {
+  // 1. Stworzenie obiektu requesta
+  const request = new XMLHttpRequest();
+
+  // 2. Konfiguracja requesta przy pomocy metody .open()
+  // 2 wymagane argumenty metody open:
+  // - metoda HTTP (np. GET)
+  // - url (endpoint jakiegoś API) z którego chcemy pobrać dane
+  request.open("GET", "http://jsonplaceholder.typicode.com/todos/");
+
+  // 3. Wysyłanie requesta (metoda .send())
+  request.send();
+
+  // 4. Oczekiwanie i reakcja na response
+  request.onload = () => {
+    // ta funkcja wykona się dopiero kiedy dotrze do nas odpowiedź od serwera
+    // console.log(request);
+    // console.log(request.responseText);
+    const data = JSON.parse(request.responseText);
+    console.log(data);
+
+  };
+};
+XMLRequestGET();
+// wywolanie
+// b) Fetch API + then chain
+// c) Fetch API + async/await
+
+
+
